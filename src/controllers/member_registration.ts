d2a81c7f08ce0b1c08dbc6e5deca3aa8885a4332
Kithsandu Rathnayake <kithsandurathnayake@gmail.com>
2023-08-08 22:54:41 +0530

    member registration api creation

--- src/controllers/member_registration.ts
+++ src/controllers/member_registration.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from "express";
-import mysql from "mysql2";
+import mysql, {
+        RowDataPacket,
+        OkPacket,
+        FieldPacket,
+} from "mysql2/promise";
 import { generateResponse } from "../utils";
+import { Connection } from "mysql2/typings/mysql/lib/Connection";
 
 const pool = mysql.createPool({
     host: "localhost",
@@ -12,13 +17,33 @@ const pool = mysql.createPool({
     queueLimit:0,
 });
 
-export const MemberRegistration = (req: Request, res: Response) => {
+
+export const MemberRegistration = async(req: Request, res: Response) => {
     try{
-        pool.getConnection((err,conection) =>{
-            if(err) {
-                console.error("Error connecting to the database:", err);
-                return res.status(500).json(generateResponse(false, null, "Database connection error"));
-            }
-        });
+        const connection = await pool.getConnection();
+        const{first_name, last_name, email, mobile_no, gender} = req.body;
+
+        const query = "INSERT INTO users(first_name, last_name, email, phone_no, gender) VALUES(?,?,?,?,?)"
+
+        const [result]: [OkPacket, FieldPacket[]] = await connection.query(query, [
+            first_name,
+            last_name,
+            email,
+            mobile_no,
+            gender
+        ]);
+
+        connection.release(); // release the connection back to the pool
+
+        // Access the inserted from the result
+        const insertedId = result.insertId;
+        // send a sucess response
+
+        res.status(200).json({message: "Data inserted successfully", insertedId,});
+        
+    }
+    catch (err){
+        console.error("Error in inserting data", err);
+        res.status(500).json(generateResponse(false, null, "Error inserting"));
     }
 };
