a73928888341d4ee0f6bdcb406a2cbde0c7f5589
Kithsandu Rathnayake <kithsandurathnayake@gmail.com>
2023-09-11 16:08:18 +0530

    Update doctor.ts

--- src/routes/manager/doctor.ts
+++ src/routes/manager/doctor.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import { getAllDoctors } from "../../controllers/manager/doctor";
+import { getAllDoctors , deleteDoctor} from "../../controllers/manager/doctor";
 
 
 
