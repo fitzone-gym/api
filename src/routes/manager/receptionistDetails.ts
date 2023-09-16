01d5a1dfb5e3f8564db0f5ea553469d6eea26ec4
JayaniRanasinghe <87237942+JayaniRanasinghe@users.noreply.github.com>
2023-09-16 20:56:54 +0530

    changed done

--- src/routes/manager/receptionistDetails.ts
+++ src/routes/manager/receptionistDetails.ts
@@ -1,10 +1,11 @@
 import express from "express"
-import { getAllReceptionists } from "../../controllers/manager/receptionist";
+import { addReceptionist, deleteReceptionist, getAllReceptionists } from "../../controllers/manager/receptionist";
 
 
 const router = express.Router()
 
 router.get("/", getAllReceptionists);
-
+router.post("/add", addReceptionist);
+router.delete("/:receptionist_id", deleteReceptionist); 
 
 export default router
