5f4a12ee118cf888036e3bae49db357231a95ec4
Dasuni Dewani <dasunidewani1998@gmail.com>
2023-10-26 20:53:24 +0530

    complete helth level and injuries

--- src/routes/trainer/memberDetails.ts
+++ src/routes/trainer/memberDetails.ts
@@ -1,12 +1,12 @@
 import express from "express"
-import { getMemberDetails, getMemberDetailsById } from "../../controllers/trainer/memberDetails";
-
+import { getMemberDetails, getMemberDetailsById ,getHelthAndInjuries} from "../../controllers/trainer/memberDetails";
 import {getWorkoutSchedule,createWorkoutSchedule, getExerciseList, deleteExersice} from "../../controllers/trainer/workoutSchedule";
 
 const router = express.Router()
 
 router.get("/:user_id", getMemberDetails); // this user_id is trainer_id 
 router.get("/memberDetails/:user_id", getMemberDetailsById)  // this user_id is member_id
+router.get("/helthInjuries/:user_id",getHelthAndInjuries);
 router.get("/schedule/:user_id", getWorkoutSchedule);
 router.post("/schedule", createWorkoutSchedule);
 router.get("/deleteSchedule/:exercise_id",deleteExersice);
