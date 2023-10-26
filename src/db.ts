92e227822491cc8f8c499aed4622191fb95e8efc
JayaniRanasinghe <87237942+JayaniRanasinghe@users.noreply.github.com>
2023-10-26 14:27:14 +0530

    payment done

--- src/db.ts
+++ src/db.ts
@@ -1,33 +1,13 @@
-// const dbConfig = {
-//   host: "fitzone.mysql.database.azure.com",
-//   user: "fitzoneAdmin",
-//   password: "gymmgnt123*",
-//   database: "fit_zone",
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// };
-
 const dbConfig = {
-  host: "localhost",
-  port:3307,
-  user: "root",
-  password: "",
-  database: "fitzone",
+  host: "fitzone.mysql.database.azure.com",
+  user: "fitzoneAdmin",
+  password: "gymmgnt123*",
+  database: "fit_zone",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 };
 
-// const dbConfig = {
-//   host: "localhost",
-//   port:3307,
-//   user: "root",
-//   password: "",
-//   database: "fitzone",
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// };
+
 
 export default dbConfig;
