31bfa43796441d753e134b4d46ece3babf8a428d
Ruwandimayunika <RuwandiMayunika01@gmail.com>
2023-10-30 21:15:52 +0530

    merge

--- src/db.ts
+++ src/db.ts
@@ -2,13 +2,11 @@ const dbConfig = {
   host: "fitzone.mysql.database.azure.com",
   user: "fitzoneAdmin",
   password: "gymmgnt123*",
+  port: 3306,
   database: "fit_zone",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 };
-
-
-
 
 export default dbConfig;
