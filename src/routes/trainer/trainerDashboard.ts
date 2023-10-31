79e42530505fae3432f1dd0de67dd9957b11f461
Dasuni Dewani <dasunidewani1998@gmail.com>
2023-11-01 04:45:29 +0530

    before diet plane

--- src/routes/trainer/trainerDashboard.ts
+++ src/routes/trainer/trainerDashboard.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import {getAvailableNotices, getMemberCount, getUpNextAppointment} from '../../controllers/trainer/trainerDashboard'
+import {getAvailableNotices, getMemberCount, getUpNextAppointment,getTodayAppointments} from '../../controllers/trainer/trainerDashboard'
 const router = express.Router();
 
 router.get('/', async(req, res) =>{
@@ -9,5 +9,6 @@ router.get('/', async(req, res) =>{
 router.get("/announcement", getAvailableNotices);
 router.get("/totalMember/:user_id", getMemberCount);
 router.get("/upNextAppointment/:user_id", getUpNextAppointment);
+router.get("/todayAppointments/:user_id", getTodayAppointments);
 
 export default router;
